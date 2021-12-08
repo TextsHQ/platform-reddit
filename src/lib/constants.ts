331c45670b1028837b85865f64cb2346ebd5b31b
Emilio Villagran <emilio@villagran.cl>
2021-12-08 18:51:44 -0300

    feat(constants): add reddit urls

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,9 +7,11 @@ export const SENDBIRD_USER_AGENT = 'Android%2Fc3.0.144'
 export const SENDBIRD_KEY = '2515BDA8-9D3A-47CF-9325-330BC37ADA13'
 
 export const RedditURLs = {
+  HOME: 'https://reddit.com',
   API_ACCOUNTS: 'https://accounts.reddit.com',
   API_OLD: 'https://old.reddit.com',
   API_OAUTH: 'https://oauth.reddit.com',
   API_S: 'https://s.reddit.com',
+  API_GRAPHQL: 'https://gql.reddit.com',
   SENDBIRD_PROXY: 'https://sendbirdproxyk8s.chat.redditmedia.com',
 }
